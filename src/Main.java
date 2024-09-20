e27ad2b36610b1af31cc6bc52f54332d9411a0e4
nicholasquad <nicholasmash2006@gmail.com>
2024-09-20 14:17:25 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int FallMaintenancecost = 100;
+        int SpringMaintenancecost = 100;
+        int AutumMaintencancecost = 100;
+        int SummerMaintenacnecost = 100;
+        int Yearlycost;
+
+        Yearlycost = FallMaintenancecost + SpringMaintenancecost + AutumMaintencancecost + SummerMaintenacnecost;
+
+        System.out.println("Your yearly maintenance cost by spending  $" + FallMaintenancecost + " for Fall $" + SpringMaintenancecost + " for Spring $" + AutumMaintencancecost + " for Autum $" + SummerMaintenacnecost + " for Summer is $" +Yearlycost  );
+    }
+
+}
+
+
